b750de0962ef3954866ef4dde81030b8564448aa
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-11-11 02:55:45 -0500

    track style done

--- frontrace/src/App.js
+++ frontrace/src/App.js
@@ -2,7 +2,7 @@ import TrackBoard from "./components/TrackBoard";
 import "./App.css";
 import dice from "./images/dice.png";
 import playerwoman from "./images/womanplayer.png";
-import playerman from "./images/womanplayer.png";
+import playerman from "./images/manplayer.png";
 import play from "./images/play.png"
 
 function App() {
@@ -11,22 +11,21 @@ function App() {
       <h1 className="Title"> CAR GAME RACE </h1>
       <div className="Buttons-container">
         <button>
-          Agregar jugadores
-          <img className="img-player" src={playerwoman} alt="playerwoman"></img>
-          <img className="img-player" src={playerman} alt="playerman"></img>
+          1. Agregar jugadores
+          <img className="img-icon" src={playerwoman} alt="playerwoman"></img>
+          <img className="img-icon" src={playerman} alt="playerman"></img>
         </button>
-        <button>Comenzar a jugar
-        <img className="img-play" src={play} alt="play"></img>
+        <button> 
+          2. Comenzar a jugar
+        <img className="img-icon" src={play} alt="play"></img>
+        </button>
+        <button>
+          3. Lanzar dados
+          <img className="img-icon" src={dice} alt="dice"></img>
         </button>
       </div>
       <div className="TrackBoard-container">
         <TrackBoard />
-      </div>
-      <div className="Buttons-container-rolldice">
-        <button>
-          Lanzar dados
-          <img className="img-dice" src={dice} alt="dice"></img>
-        </button>
       </div>
     </div>
   );
--- frontrace/src/components/TrackBoard.js
+++ frontrace/src/components/TrackBoard.js
@@ -1,21 +1,17 @@
 import React from "react";
+import meta from "../images/meta.png"
 
 import "./TrackBoard.css";
 
-const verticalAxis = ["1","2","3","4","5","6","7","8"];
-const horizontalAxis = ["A","B","C","D","E","F","G","H"];
 
 export default function TrackBoard() {
   let board = [];
-  let numberOfLanes =5;
-  let trackLength=5;
+  let numberOfLanes =3;
+  let trackLength=10;
 
-  for (let i=0; i < horizontalAxis.length; i++) {
-    for (let j=0; j < verticalAxis.length; i++) {
-      board.push(<span>{horizontalAxis[i]} {verticalAxis[j]}</span>)
-    }
-  }
-
-
-  return <div className="Board-container">Here is the track!</div>;
+  return <div className="Board-container">
+    <div className="Carril">Carril 1 <div className="meta-container"> </div>  </div>
+    <div className="Carril">Carril 2 <div className="meta-container">  </div></div>
+    <div className="Carril">Carril 3 <div className="meta-container">   </div></div>
+  </div>;
 }
